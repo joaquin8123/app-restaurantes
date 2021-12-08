d1602c42b943807be2db5e98e65694f54abe3942
Joaquin Suarez <joaquin8123@gmail.com>
2021-12-08 20:32:46 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { YellowBox } from "react-native";
+import { firebaseApp } from "./app/utils/firebase";
+import Navigation from "./app/navigations/Navigation";
+import { decode, encode } from "base-64";
+
+YellowBox.ignoreWarnings(["Setting a timer"]);
+
+if (!global.btoa) global.btoa = encode;
+if (!global.atob) global.atob = decode;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
